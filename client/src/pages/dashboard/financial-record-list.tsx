3b77bd8d865b6bc30a45f24c675e07c6a70f2a23
Charles Chung <charlesgnuhc@gmail.com>
2024-10-24 00:16:37 +0800

    feature: add financial-record-list

--- client/src/pages/dashboard/financial-record-list.tsx
+++ client/src/pages/dashboard/financial-record-list.tsx
@@ -1,7 +1,145 @@
-export const FinancialRecordList = () => {
+import { useMemo, useState } from "react";
+import { FinancialRecord, useFinancialRecords } from "../../contexts/financial-record-context";
+import { useTable, Column, CellProps, Row } from 'react-table';
+
+interface EditableCellProps extends CellProps<FinancialRecord> {
+    updateRecord: (rowIndex: number, columnId: string, value: any) => void;
+    editable: boolean;
+}
+
+const EditableCell: React.FC<EditableCellProps> = ({
+        value: initialValue, 
+        row, 
+        column, 
+        updateRecord, 
+        editable
+    }) => {
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [value, setValue] = useState(initialValue);
+
+    const onBlur = () => {
+        setIsEditing(false);
+        updateRecord(row.index, column.id, value);
+    };
+
     return (
-    <div>
-        Record List
+        <div 
+            onClick={() => editable && setIsEditing(true)} 
+            style={{cursor: editable ? "pointer" : "default"}}>
+            {isEditing ? 
+                <input 
+                    value={value} 
+                    onChange={(e) => setValue(e.target.value)}
+                    autoFocus
+                    onBlur={onBlur}
+                    style={{width: "100%"}}
+                /> 
+                : typeof value === "string" ? (value)
+                : (value.toString)
+            }                
+        </div>
+    )
+}
+
+export const FinancialRecordList = () => {
+    const { records } = useFinancialRecords();
+
+    const columns : Array<Column<FinancialRecord>> = useMemo(() => [
+        {
+            Header: "Description",
+            accessor: "description",
+            Cell: (props) => (
+                <EditableCell {...props} updateRecord={() => null} editable={true}/>
+            )
+        },
+        {
+            Header: "Amount",
+            accessor: "amount",
+            Cell: (props) => (
+                <EditableCell {...props} updateRecord={() => null} editable={true}/>
+            )
+        },
+        {
+            Header: "Category",
+            accessor: "category",
+            Cell: (props) => (
+                <EditableCell {...props} updateRecord={() => null} editable={true}/>
+            )
+        },
+        {
+            Header: "OnlineFlag",
+            accessor: "onlineflag",
+            Cell: (props) => (
+                <EditableCell {...props} updateRecord={() => null} editable={true}/>
+            )
+        },
+        {
+            Header: "OverseasFlag",
+            accessor: "overseasflag",
+            Cell: (props) => (
+                <EditableCell {...props} updateRecord={() => null} editable={true}/>
+            )
+        },
+        {
+            Header: "CreditCard",
+            accessor: "creditcard",
+            Cell: (props) => (
+                <EditableCell {...props} updateRecord={() => null} editable={true}/>
+            )
+        },
+        {
+            Header: "Date",
+            accessor: "date",
+            Cell: (props) => (
+                <EditableCell {...props} updateRecord={() => null} editable={false}/>
+            )
+        },
+        {
+            Header: "Delete",
+            id: "delete",
+            Cell: ({ row }) => (
+                <button
+                  onClick={() => deleteRecord(row.original._id ?? "")}
+                  className="button"
+                >
+                  Delete
+                </button>
+            )
+        },
+    ], []);
+
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = 
+        useTable({
+            columns, 
+            data: records 
+        });
+
+    return (
+    <div className = "table-container">
+        <table {...getTableProps()} className="table">
+            <thead>
+                {headerGroups.map((hg) => (
+                    <tr {...hg.getHeaderGroupProps()}>
+                        {hg.headers.map((column) => (
+                            <th {...column.getHeaderProps()}> {column.render("Header")} </th>
+                        ))}
+                    </tr>
+                ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {rows.map((row, idx) => {
+                    prepareRow(row);
+                    return <tr {...row.getRowProps()}> {row.cells.map((cell) => (
+                        <td {...cell.getCellProps}> {cell.render("Cell")} </td>
+                    ))}</tr>
+                })}
+            </tbody>
+        </table>
     </div>
     )
+}
+
+function deleteRecord(arg0: any): void {
+    throw new Error("Function not implemented.");
 }
