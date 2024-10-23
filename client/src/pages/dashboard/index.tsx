41ce337f262a03c343d4b1a0e98bcb013d2f39de
Charles Chung <charlesgnuhc@gmail.com>
2024-10-24 00:20:45 +0800

    feature: make things look better @css

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -1,5 +1,7 @@
 import { useUser } from '@clerk/clerk-react'
 import { FinancialRecordForm } from './financial-record-form';
+import { FinancialRecordList } from './financial-record-list';
+import "./financial-record.css";
 
 export const Dashboard = () => {
     const { user } = useUser();
@@ -7,6 +9,7 @@ export const Dashboard = () => {
         <div className="dashboard-container">
             <h1>Welcome { user?.firstName }! Here are your finances:</h1>
             <FinancialRecordForm/>
+            <FinancialRecordList/>
         </div>
-    )
+    );
 }
